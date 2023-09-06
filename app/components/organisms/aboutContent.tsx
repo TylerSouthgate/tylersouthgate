016932c73c726fc8a08cbb916ef5dedf3c8a22b5
TylerSouthgate2112 <tylersouthgate@hotmail.com>
2023-09-06 12:19:38 +0100

    update

--- app/components/organisms/aboutContent.tsx
+++ app/components/organisms/aboutContent.tsx
@@ -3,18 +3,23 @@ const AboutContent = () => {
     <div className="relative z-[40] border p-4 mt-4 rounded-lg shadow-xl tile">
       <p>
         Born and brought up in the town of Ipswich, Suffolk in the UK on or
-        around the 1970's. TS managed to avoid all the normal things that people
-        did, like getting a job or a pension and instead pursued a life of nail
-        biting unpredictability. There where bands and guitars, a little too
-        much Wiliam Gibson at times, and way too many horses.
+        around the 1970&#39;s. TS managed to avoid all the normal things that
+        people did, like getting a job or a pension and instead pursued a life
+        of nail biting unpredictability.
       </p>
       <p>
-        The years where packed with couch surfing, penny scrounging DJ'ing and
-        twenty foot mural painting. Somewhere along the way he learned how to
-        code and went with the flow all the way to India. He finally made his
+        There where bands and guitars, bikes and cars. Mad hat trips around
+        Europe and a gig at a collage in Germany, which was very strange. A
+        little too much Wiliam Gibson, a record by the Police, a bandana that
+        was way too small (gave him a headache) and way too many horses.
+      </p>
+      <p>
+        The years where packed with couch surfing, penny scrounging DJ&#39;ing
+        and twenty foot mural painting. Somewhere along the way he learned how
+        to code and went with the flow all the way to India. He finally made his
         way home, some twenty years later, and is now tending a Victorian house
         in a small market town on the Norfolk borders with his loving wife and
-        irritating daughter (just kidding, she's lovely really!).
+        irritating daughter (just kidding, she&#39;s lovely really!).
       </p>
       <h2>Likes?</h2>
       <ul>
