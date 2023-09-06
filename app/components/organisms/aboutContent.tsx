eaa45adf8626a2b9f4eebb3b4ab7e47845ae53b6
TylerSouthgate2112 <tylersouthgate@hotmail.com>
2023-09-06 12:33:13 +0100

    update

--- app/components/organisms/aboutContent.tsx
+++ app/components/organisms/aboutContent.tsx
@@ -2,25 +2,25 @@ const AboutContent = () => {
   return (
     <div className="relative z-[40] border p-4 mt-4 rounded-lg shadow-xl tile">
       <p>
-        Born and brought up in the town of Ipswich, Suffolk in the UK on or
-        around the 1970&#39;s. TS managed to avoid all the normal things that
-        people did, like getting a job or a pension and instead pursued a life
-        of nail biting unpredictability.
+        Born and brought up in Ipswich, a toen in the heart of Suffolk, England
+        in or around the 1970&#39;s. TS managed to avoid all the normal things
+        that people did, like getting a job or a pension and instead pursued a
+        life of nail biting unpredictability.
       </p>
       <p>
-        There where bands and guitars, bikes and cars. Mad hat trips around
+        There were bands and guitars, bikes and cars. Mad hat trips around
         Europe and a gig at a collage in Germany, which was very strange. A
-        little too much Wiliam Gibson, a record by the Police, a bandana that
-        was way too small (gave him a headache) and way too many horses.
+        little too much Wiliam Gibson, something by the Police, bandana&#39;s
+        that were way too small (they gave him a headache), jeans from a pear in
+        SanFran and too many horses. Really, WAY too many horses.
       </p>
       <p>
-        The years where packed with couch surfing, penny scrounging DJ&#39;ing
+        The years were packed with couch surfing, penny scrounging DJ&#39;ing
         and twenty foot mural painting. Somewhere along the way he learned how
-        to code and went with the flow. A flow that went all the way to India.
-        He finally made his way home, some twenty years later, and is now
-        tending an old Victorian house in a small market town on the Norfolk
-        borders with his loving wife and irritating daughter (just kidding,
-        she&#39;s lovely really!).
+        to code and went with the flow. A flow that went, as it turns out, all
+        the way to India. He finally made his way home, some twenty years later,
+        and is now tending an old Victorian house in a small market town on the
+        Norfolk borders with his loving wife and daughter.
       </p>
       <h2>Likes?</h2>
       <ul>
